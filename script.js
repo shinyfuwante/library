a2f7b619a09e92696abcb909fca299651c1d0cd3
shinyfuwante <mr.philliptang@gmail.com>
2022-04-22 17:50:29 -0700

    Update displayLibrary() to reset display

--- script.js
+++ script.js
@@ -40,12 +40,13 @@ function createCardHelper(string, bookField) {
 
 
 function displayLibrary() {
-    //myLibrary.forEach(book => console.log(book));
-    //select the library element (which will be a grid)
-    //loop through library array and for each book ->
-    //append createBookCard(book)
-    //display the card by appending it to library!
     const library = document.querySelector("library");
+
+    let child = library.lastChild;
+    while (child) {
+        library.removeChild(child);
+        child = library.lastChild;
+    }
     myLibrary.forEach(book => library.appendChild(createBookCard(book)));
 }
 
@@ -65,13 +66,10 @@ function newBook(e) {
     let bookAuthor = document.querySelector('#book_author');
     let bookPages = document.querySelector('#book_pages');
     let readStatus = document.querySelector('#have_read');
-    console.log(bookName.value);
-    console.log(bookAuthor.value);
-    console.log(bookPages.value);
-    console.log(readStatus.checked);
 
     let bookToAdd = new Book(bookAuthor.value, bookName.value, bookPages.value, readStatus.checked);
     addBook(bookToAdd);
+    displayLibrary();
     
 }
 // -------------------------------
